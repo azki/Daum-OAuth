964857207c26692bad578d7e94eb45e088cbd415
unknown <azki@.(none)>
2010-08-08 01:19:26 +0900

    first commit

---
+++ daumOAuth.php
@@ -0,0 +1,147 @@
+<?php
+/*
+ * Azki (azki@azki.org) http://azki.org/
+ *
+ * Basic lib to work with Daum OAuth.
+ *
+ * Daum OAuth page: https://apis.daum.net/oauth/main/welcome
+ *
+ * Code based on:
+ * Fire Eagle code - http://github.com/myelin/fireeagle-php-lib
+ * twitterlibphp - http://github.com/poseurtech/twitterlibphp
+ * twitterOAuth.php - http://abrah.am/
+ * 
+ * Version: 1.0.0
+ * Last Update: 2010-08-04
+ */
+
+/* Load OAuth lib. You can find it at http://oauth.net */
+require_once('OAuth.php');
+
+/**
+ * Daum OAuth class
+ */
+class DaumOAuth {/*{{{*/
+	/* Contains the last HTTP status code returned */
+	private $http_status;
+
+	/* Contains the last API call */
+	private $last_api_call;
+
+	/* Set up the API root URL */
+	public static $TO_API_ROOT = "https://apis.daum.net";
+
+	/**
+	 * Set API URLS
+	 */
+	function requestTokenURL() { return self::$TO_API_ROOT.'/oauth/requestToken'; }
+	function authorizeURL() { return self::$TO_API_ROOT.'/oauth/authorize'; }
+	function accessTokenURL() { return self::$TO_API_ROOT.'/oauth/accessToken'; }
+
+	/**
+	 * Debug helpers
+	 */
+	function lastStatusCode() { return $this->http_status; }
+	function lastAPICall() { return $this->last_api_call; }
+
+	/**
+	 * construct DaumOAuth object
+	 * 
+	 * @example
+	 * new DaumOAuth(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL);
+	 * new DaumOAuth(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL, OAUTH_TOKEN, OAUTH_TOKEN_SECRET); 
+	 */
+	function __construct($consumer_key, $consumer_secret, $callback_url, $oauth_token = NULL, $oauth_token_secret = NULL) {/*{{{*/
+		$this->sha1_method = new OAuthSignatureMethod_HMAC_SHA1();
+		$this->consumer = new OAuthConsumer($consumer_key, $consumer_secret, $callback_url);
+		if (!empty($oauth_token) && !empty($oauth_token_secret)) {
+			$this->token = new OAuthConsumer($oauth_token, $oauth_token_secret, $callback_url);
+		} else {
+			$this->token = NULL;
+		}
+	}/*}}}*/
+
+	/**
+	 * Get a request_token from Daum
+	 */
+	function getRequestToken() {/*{{{*/
+		$r = $this->oAuthRequest($this->requestTokenURL(), array('oauth_callback'=>$this->consumer->callback_url), 'GET');
+		$token = $this->oAuthParseResponse($r);
+		$this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret'], $this->consumer->callback_url);
+		return $token;
+	}/*}}}*/
+
+	/**
+	 * Parse a URL-encoded OAuth response
+	 */
+	function oAuthParseResponse($responseString) {
+		$r = array();
+		foreach (explode('&', $responseString) as $param) {
+			$pair = explode('=', $param, 2);
+			if (count($pair) != 2) continue;
+			$r[urldecode($pair[0])] = urldecode($pair[1]);
+		}
+		return $r;
+	}/*}}}*/
+
+	/**
+	 * Get the authorize URL
+	 */
+	function getAuthorizeURL($token) {/*{{{*/
+		if (is_array($token)) $token = $token['oauth_token'];
+		return $this->authorizeURL() . '?oauth_token=' . $token;
+	}/*}}}*/
+
+	/**
+	 * Exchange the request token and secret and verifier for an access token and secret, to sign API calls.
+	 */
+	function getAccessToken($verifier) {/*{{{*/
+		//$r = $this->oAuthRequest($this->accessTokenURL());
+		$r = $this->oAuthRequest($this->accessTokenURL(), array('oauth_callback'=>$this->consumer->callback_url, 'oauth_verifier'=>$verifier), 'GET');
+		$token = $this->oAuthParseResponse($r);
+		$this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret'], $this->consumer->callback_url);
+		return $token;
+	}/*}}}*/
+
+	/**
+	 * Format and sign an OAuth / API request
+	 */
+	function oAuthRequest($url, $args = array(), $method = NULL) {/*{{{*/
+		if (empty($method)) {
+			$method = 'GET';
+		}
+		$req = OAuthRequest::from_consumer_and_token($this->consumer, $this->token, $method, $url, $args);
+		$req->sign_request($this->sha1_method, $this->consumer, $this->token);
+		switch ($method) {
+		case 'GET':
+			return $this->http($req->to_url());
+		case 'POST':
+			return $this->http($req->get_normalized_http_url(), $req->to_postdata());
+		}
+	}/*}}}*/
+
+	/**
+	 * Make an HTTP request
+	 */
+	function http($url, $post_data = null) {/*{{{*/
+		$ch = curl_init();
+		if (defined("CURL_CA_BUNDLE_PATH")) curl_setopt($ch, CURLOPT_CAINFO, CURL_CA_BUNDLE_PATH);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		//////////////////////////////////////////////////
+		///// Set to 1 to verify Twitter's SSL Cert //////
+		//////////////////////////////////////////////////
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		if (isset($post_data)) {
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		}
+		$response = curl_exec($ch);
+		$this->http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$this->last_api_call = $url;
+		curl_close ($ch);
+		return $response;
+	}/*}}}*/
+}/*}}}*/
---
+++ index.phps
@@ -0,0 +1,120 @@
+<?php
+require_once('./daumOAuth.php');
+
+session_start();//for save request token.
+
+$consumer_key = '090cb499-6e91-488c-bb96-fbf4be7d2ae0';
+$consumer_secret = 'T7jB3zXmcH.J76X5wkfKy1biG4My4PNNl4r.TZ9ZHw5.S1wRz9GSRg00';
+$callback_url = 'http://daumoauth.azki.org/';
+
+$session_var_names = array('request_token', 'user_auth_url', 'access_token', 'blog_info');
+
+$o_mode = @$_REQUEST['o_mode'];
+switch ($o_mode) {
+	default:
+		//처음 접속시 or 2번후 콜백으로 들어와서 3번 진행.
+		$o_token = @$_REQUEST['oauth_token'];
+		$o_verifier = @$_REQUEST['oauth_verifier'];
+		if (isset($o_token) && isset($o_verifier)) {
+			//2번에서 인증 후에 콜백 URL로 다시 돌아오는 곳.
+			$rTok = $_SESSION['request_token'];
+			$to = new DaumOAuth($consumer_key, $consumer_secret, $callback_url, $o_token, $rTok['oauth_token_secret']);
+			$aTok = @$to->getAccessToken($o_verifier);
+			$_SESSION['access_token'] = $aTok;
+		}
+		else {
+			//처음 접속시.
+			foreach ($session_var_names as $var_name) {
+				if (isset($_SESSION[$var_name])) {
+					unset($_SESSION[$var_name]);
+				}
+			}
+		}
+		break;
+	case 'request_token':
+		//1번의 버튼을 눌러서 진행됨.
+		$to = new DaumOAuth($consumer_key, $consumer_secret, $callback_url);
+		$rTok = $to->getRequestToken();
+		$url = $to->getAuthorizeURL($rTok);
+		$_SESSION['request_token'] = $rTok;
+		$_SESSION['user_auth_url'] = $url;
+		break;
+	case 'blog_info':
+		//3번의 버튼을 눌러서 진행됨.
+		$aTok = @$_SESSION['access_token'];
+		$blog_id = $_REQUEST['blog_id'];
+		if (isset($aTok)) {
+			$to = new DaumOAuth($consumer_key, $consumer_secret, $callback_url, $aTok['oauth_token'], $aTok['oauth_token_secret']);
+			$url = 'http://apis.daum.net/blog/info/blog.do';
+			$args = array('blogName'=>$blog_id);
+			$response = $to->OAuthRequest($url, $args);
+			$_SESSION['blog_info'] = $response;
+		}
+		break;
+}
+
+$dump_data = array();
+foreach ($session_var_names as $var_name) {
+	if (isset($_SESSION[$var_name])) {
+		$dump_data[$var_name] = $_SESSION[$var_name];
+	}
+}
+?><!DOCTYPE html> 
+<html> 
+<head>
+<meta charset=utf-8><!-- simplified version; works on legacy browsers -->
+<title>Daum OAuth Example.</title>
+</head>
+<body>
+	<h1>Daum OAuth Example.</h1>
+	<textarea rows="10" cols="120"><?php print_r($dump_data); ?></textarea>
+	<hr/>
+	<p>
+		<h3>0. 정보 초기화 하기.</h3>
+		<form>
+			<input name="o_mode" value="reset" type="hidden" />
+			<input type="submit" />
+		</form>
+	</p>
+	<p>
+		<h3>1. 인증되지 않은 Request 토큰 획득 및 서비스 프로바이더의 사용자 인증 URL 구하기.</h3>
+		<form>
+			<input name="o_mode" value="request_token" type="hidden" />
+			<input type="submit" />
+		</form>
+	</p>
+	<p>
+		<h3>2. 사용자 인증 획득 및 Access 토큰 획득</h3>
+		1번에서 받은 user_auth_url 로 이동하여 인증하십시요.
+		<div>
+			<?php 
+if (isset($_SESSION['user_auth_url'])) {
+	$url = $_SESSION['user_auth_url'];
+	print '<a href="' . $url . '">' . $url . '</a>';
+}
+			?>
+		</div>
+	</p>
+	<p>
+		<h3>3. 보호된 리소스에 접근</h3>
+		다음 오픈 API 중 "블로그 정보 가져오기"의 예제(http://apis.daum.net/blog/info/blog.do).
+		<form>
+			<input name="o_mode" value="blog_info" type="hidden" />
+			블로그 아이디: <input name="blog_id" value="<?php
+print isset($blog_id) ? $blog_id : 'ahahblog';		
+			?>" type="text" />
+			<br/><input type="submit" />
+		</form>
+		<div>
+			<pre><?php
+if (isset($_SESSION['blog_info'])) {
+	$info = $_SESSION['blog_info'];
+	print htmlspecialchars($info);
+}
+			?></pre>
+		</div>
+	</p>
+	<hr/>
+	<addr>Powered by <a href="http://github.com/azki/Daum-OAuth">http://github.com/azki/Daum-OAuth</a></addr>
+</body>
+</html>
